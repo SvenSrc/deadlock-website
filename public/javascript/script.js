1630798bda99c5088663ea0c1f383a03026055ce
Sven <sven_beller@web.de>
2024-10-03 23:36:21 +0200

    Images are added dynamically

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -8,10 +8,21 @@ fetch("http://localhost:3000/")
         outputData(data)
     });
 
+// Crate images
 function outputData(data){
-    data.forEach(hero =>
-        document.getElementById("heroList").innerHTML;
-    )
+    for(var i = 0; i < data.length; i++){
+        console.log(data[i].name);
+        
+        if(data[i].player_selectable == true && data[i].in_development == false && data[i].disabled == false){
+            const node = document.createElement("img");
+            node.src = data[i].images.card;
+    
+            document.getElementById("heroes").appendChild(node)  
+        }      
+    }
 }
+
+/*     data.forEach(hero =>
+        document.getElementById("heroList").innerHTML); */
 
   
